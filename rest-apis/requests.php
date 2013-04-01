73387086e15cc6ebad41497306dad52405d4e839
Jonathan LeBlanc <joncleblanc@yahoo.com>
2013-04-01 11:28:23 -0700

    added complete list of GET/POST request functionality from new REST APIs

--- rest-apis/requests.php
+++ rest-apis/requests.php
@@ -37,6 +37,43 @@ class paypal{
     }
     
     /**
+    * Process Approved Payment
+    *
+    * Processes an approved PayPal or credit card payment
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#execute-an-approved-paypal-payment
+    */
+    public function process_approved_payment($id, $request){
+        $postvals = $request;
+        $uri = URI_SANDBOX . "payments/payment/$id/execute";
+        return self::curl($uri, 'POST', $postvals);
+    }
+    
+    /**
+    * Fetch Payments
+    *
+    * Returns a series of previous payments made to the application account
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#list-payment-resources
+    */
+    public function fetch_payments($args){
+        $query_string = '';
+        if (count($args) > 0){ $query_string = '?' . http_build_query($args); }
+        
+        $uri = URI_SANDBOX . "payments/payment" . $query_string;
+        return self::curl($uri, 'GET');
+    }
+    
+    /**
+    * Fetch Single Payment
+    *
+    * Returns a single payment made to the application account
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#look-up-a-payment-resource
+    */
+    public function fetch_single_payment($id){
+        $uri = URI_SANDBOX . "payments/payment/$id";
+        return self::curl($uri, 'GET');
+    }
+    
+    /**
     * Refund Sale
     *
     * Refunds a previous payment
@@ -45,6 +82,17 @@ class paypal{
     public function refund_sale($sale_id){
         $uri = URI_SANDBOX . "payments/sale/$sale_id/refund";
         return self::curl($uri, 'POST', '{}');
+    }
+    
+    /**
+    * Fetch Refund
+    *
+    * Returns a single refund details object
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#look-up-a-refund
+    */
+    public function fetch_refund($id){
+        $uri = URI_SANDBOX . "refund/$id";
+        return self::curl($uri, 'GET');
     }
     
     /**
@@ -59,6 +107,17 @@ class paypal{
     }
     
     /**
+    * Fetch Credit Card
+    *
+    * Fetches a credit card object from the vault
+    * @link https://developer.paypal.com/webapps/developer/docs/api/#look-up-a-stored-credit-card
+    */
+    public function store_cc($cc_id){
+        $uri = URI_SANDBOX . "vault/credit-card/$cc_id";
+        return self::curl($uri, 'GET');
+    }
+    
+    /**
     * cURL
     *
     * Handles GET / POST requests for auth requests
@@ -66,24 +125,24 @@ class paypal{
     */
     private function curl($url, $method = 'GET', $postvals = null, $auth = false){
         $ch = curl_init($url);
+        
+        //if we are sending request to obtain bearer token
+        if ($auth){
+            $headers = array("Accept: application/json", "Accept-Language: en_US");
+            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+            curl_setopt($ch, CURLOPT_USERPWD, CLIENT_ID . ":" .CLIENT_SECRET);
+            curl_setopt($ch, CURLOPT_SSLVERSION, 3);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+        //if we are sending request with the bearer token for protected resources
+        } else {
+            $headers = array("Content-Type:application/json", "Authorization:{$this->token_type} {$this->access_token}");
+        }
            
         if ($method == 'GET'){
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         } else {
-            //if we are sending request to obtain bearer token
-            if ($auth){
-                $headers = array("Accept: application/json", "Accept-Language: en_US");
-                curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-                curl_setopt($ch, CURLOPT_USERPWD, CLIENT_ID . ":" .CLIENT_SECRET);
-                curl_setopt($ch, CURLOPT_SSLVERSION, 3);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-                curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
-            //if we are sending request with the bearer token for protected resources
-            } else {
-                $headers = array("Content-Type:application/json", "Authorization:{$this->token_type} {$this->access_token}");
-            }
-            
             $options = array(
                 CURLOPT_HEADER => true,
                 CURLINFO_HEADER_OUT => true,
